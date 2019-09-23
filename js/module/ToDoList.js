d20acb0c8340e8fba6b11b1e56e078d02bb7e16b
guihenrry <guilhermehenry60@gmail.com>
2019-09-22 21:51:27 -0300

    adicionado funcionamento basico do todolist com javascript

--- js/module/ToDoList.js
+++ js/module/ToDoList.js
@@ -4,11 +4,91 @@ export default class ToDoList {
     this.buttonAdd = document.querySelector(buttonAdd);
     this.list = document.querySelector(list);
 
-    this.arrayList = [];
+    this.textArray = [];
+
+    this.addTextByInput = this.addTextByInput.bind(this);
+    this.handleKeyup = this.handleKeyup.bind(this);
   }
 
   add(text) {
-    this.arrayList.push(text);
+    this.textArray.push(text);
+  }
+
+  remove(index) {
+    this.textArray.splice(index, 1);
+  }
+
+  addRemoveEvent(element) {
+    const buttonRemove = element.querySelector('.todo-remove');
+    const indexElement = buttonRemove.getAttribute('data-todo');
+
+    buttonRemove.addEventListener('click', () => {
+      element.remove();
+      this.remove(indexElement);
+      this.createListByArray();
+    });
+  }
+
+  createNewElement(text, index) {
+    const div = document.createElement('div');
+    div.classList.add('todo-item');
+
+    div.innerHTML = `
+      <p class="todo-text">${text}</p>
+      <button data-todo="${index}" class="todo-remove">X</button>
+    `;
+
+    this.addRemoveEvent(div);
+    return div;
+  }
+
+  updateLocalStorage() {
+    window.localStorage.textArray = JSON.stringify(this.textArray);
+  }
+
+  createListByArray() {
+    this.list.innerHTML = '';
+
+    this.textArray.forEach((text, index) => {
+      const item = this.createNewElement(text, index);
+      this.list.appendChild(item);
+    });
+
+    this.updateLocalStorage();
+  }
+
+  addTextByInput() {
+    if (this.input.value !== '') {
+      this.add(this.input.value);
+      this.input.value = '';
+      this.createListByArray();
+    }
+  }
+
+  handleKeyup({key}) {
+    if (key === 'Enter') this.addTextByInput();
+  
+  }
+
+  getLocalStorage() {
+    if (window.localStorage.textArray) {
+      this.textArray = JSON.parse(window.localStorage.textArray);
+      this.createListByArray();
+    }
+  } 
+
+  addToDoEvents() {
+    this.buttonAdd.addEventListener('click', this.addTextByInput);
+
+    document.addEventListener('keyup', this.handleKeyup);
+  }
+
+  init() {
+    if (this.input && this.buttonAdd && this.list) { 
+      this.addToDoEvents();
+      this.getLocalStorage();
+    }
+    return this;
   }
 
 }
--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
 import ToDoList from './module/ToDoList.js';
 
 const toDoList = new ToDoList('[data-todo="input"]', '[data-todo="add"]', '[data-todo="list"]');
+
+toDoList.init();
